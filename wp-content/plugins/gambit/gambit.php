11523f3f0f7e6cfac8d4f8446d5408714e589f19
Aaron Schachter <aaronschachter@gmail.com>
2016-10-11 23:08:50 -0700

    Adds custom Gambit plugin to filter default WP properties from API response

---
+++ wp-content/plugins/gambit/gambit.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: Gambit
+Description: Custom plugin to modify WP REST API responses.
+Author: DoSomething.org
+Version: 1.0
+*/
+
+/**
+ * Removes and alters default WP REST API post properties for given $data.
+ */
+function gambit_filter_bot_json($data, $post, $context) {
+  $remove = [
+    'content',
+    'date',
+    'date_gmt',
+    'guid',
+    'link',
+    'modified',
+    'modified_gmt',
+    'slug',
+    'tags',
+  ];
+  foreach ($remove as $property) {
+    unset($data->data[$property]);
+  }
+
+  // Unsetting type doesn't work in our foreach loop, so we unset here.
+  unset($data->data['type']);
+  $data->data['title'] = $data->data['title']['rendered'];
+
+  return $data;
+}
+
+add_filter( 'rest_prepare_campaignbot', 'gambit_filter_bot_json', 10, 3 );
+add_filter( 'rest_prepare_donorschoosebot', 'gambit_filter_bot_json', 10, 3 );
+
+?>
